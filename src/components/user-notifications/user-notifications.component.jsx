88bb64d26174aa3c78b2ab15b52b9c2f3a895054
divyanshthakurx <divyanshthakurx@gmail.com>
2024-09-18 16:49:50 +0530

    cli errors fixed v1.0

--- src/components/user-notifications/user-notifications.component.jsx
+++ src/components/user-notifications/user-notifications.component.jsx
@@ -21,7 +21,7 @@ const UserNotifications = () => {
                             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400">
                                     <tr className="text-center">                                      
-                                        <th scope="col" className="px-6 py-3">
+                                        <th scope="col" className="px6 py-3">
                                             Book Name
                                         </th>
                                         <th scope="col" className="px-6 py-3">
